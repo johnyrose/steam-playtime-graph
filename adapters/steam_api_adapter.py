bcc1041db4625c47bf7e8f324c9e5199828bcc3b
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-13 18:40:33 +0200

    Add get_owned_games function to Steam API adapter.

--- adapters/steam_api_adapter.py
+++ adapters/steam_api_adapter.py
@@ -6,3 +6,8 @@ def get_player_id_from_profile_name(profile_name: str, steam_api_key: str) -> in
     steamid = user_info.resolve_vanity_url(profile_name)['response']['steamid']
     return steamid
 
+
+def get_owned_games(steam_id: int, steam_api_key: str) -> list:
+    player_service = IPlayerService(steam_api_key=steam_api_key)
+    games = player_service.get_owned_games(steam_id)
+    return games
