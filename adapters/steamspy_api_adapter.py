ce459d5014b23bfbc9047a0dc88e39b467f8dc4e
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-13 19:04:29 +0200

    Fix get_game_info to return dict object instead of string.

--- adapters/steamspy_api_adapter.py
+++ adapters/steamspy_api_adapter.py
@@ -1,7 +1,8 @@
 from requests import get
 from config import config
+import json
 
 
 def get_game_info(game_id: int) -> dict:
     response = get(f'{config.STEAMSPY_URL}/api.php?request=appdetails&appid={game_id}')
-    return response.text
+    return json.loads(response.text)
