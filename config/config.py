df104f686dce2af48305b6bed2c166387fcef006
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-13 21:34:05 +0200

    Add used functions to main

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 STEAMSPY_URL = "http://steamspy.com"
 PROFILE_NAME = "your_profile"
 STEAM_API_KEY = "your_key"
-MAX_STUDIOS_AMOUNT = 10
+MAX_STUDIOS_AMOUNT = 15
 MINIMUM_HOURS = 5
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from logic import player_stats_creator, graph_creator
+from logic import player_stats_creator, graph_creator, extended_names_unifier, results_filter
 
 
 if __name__ == '__main__':
     stats = player_stats_creator.create_stats_by_developer()
+    stats = extended_names_unifier.unify_extended_names(stats)
+    stats = results_filter.filter_results(stats)
     graph_creator.generate_stats_bar_graph(stats)
