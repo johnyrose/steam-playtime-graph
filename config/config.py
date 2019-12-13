47a8bbc415ac2746d276401935520cd689c7d6a9
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-13 22:06:43 +0200

    Add config reading from JSON file.

--- config/config.py
+++ config/config.py
@@ -1,5 +1,18 @@
+from config.config_loader import load_config
+
+# region Static configuration
+
 STEAMSPY_URL = "http://steamspy.com"
-PROFILE_NAME = "your_profile"
-STEAM_API_KEY = "your_key"
-MAX_STUDIOS_AMOUNT = 15
-MINIMUM_HOURS = 5
+CONFIG_FILE = "config.json"
+
+# endregion
+
+# region User configuration
+user_config = load_config(CONFIG_FILE)
+
+PROFILE_NAME = user_config["steam_profile_name"]
+STEAM_API_KEY = user_config["steam_api_key"]
+MAX_STUDIOS_AMOUNT = int(user_config.get("max_bars_amount", 15))
+MINIMUM_HOURS = int(user_config.get("minimum_hours", None))
+
+# endregion
