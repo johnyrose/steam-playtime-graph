7f5c2958899b969e2e40a6ba78be14fba4bb0f67
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-13 19:08:19 +0200

    Move dict creation to a separate generic function.

--- logic/player_stats_creator.py
+++ logic/player_stats_creator.py
@@ -2,13 +2,18 @@ from config import config
 from adapters import steam_api_adapter, steamspy_api_adapter
 
 
+def _create_stats_by_common_game_info_attribute(games: list, attribute: str) -> dict:
+    stats_dict = {}
+    for game_stats in games:
+        game_info = steamspy_api_adapter.get_game_info(game_stats['appid'])
+        if game_info[attribute] not in stats_dict:
+            stats_dict[game_info[attribute]] = 0
+        stats_dict[game_info[attribute]] += game_stats['playtime_forever']
+    return stats_dict
+
+
 def create_stats_by_developer() -> dict:
     profile_id = steam_api_adapter.get_player_id(config.PROFILE_NAME, config.STEAM_API_KEY)
     games = steam_api_adapter.get_owned_games(profile_id, config.STEAM_API_KEY)
-    devs_dict = {}
-    for game_stats in games:
-        game_info = steamspy_api_adapter.get_game_info(game_stats['appid'])
-        if game_info['developer'] not in devs_dict:
-            devs_dict[game_info['developer']] = 0
-        devs_dict[game_info['developer']] += game_stats['playtime_forever']
+    devs_dict = _create_stats_by_common_game_info_attribute(games, attribute='developer')
     return devs_dict
