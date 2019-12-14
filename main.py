7bb0e44a36d6eb87c2d18d6af5063367285731e0
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-14 11:41:26 +0200

    Complete main to use new functionality.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from logic import player_stats_creator, graph_creator, extended_names_unifier, results_filter
+from logic import graph_creator, extended_names_unifier, results_filter, measuring_function_finder
+from config import config
+
 
 if __name__ == '__main__':
-    stats = player_stats_creator.create_stats_by_developer()
+    measuring_function = measuring_function_finder.get_stats_measuring_function(config.MEASURE_BY)
+    stats = measuring_function()
     stats = extended_names_unifier.unify_extended_names(stats)
     stats = results_filter.filter_results(stats)
     graph_creator.generate_stats_bar_graph(stats)
