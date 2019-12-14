52cd58e71f195e1ffb63254bee1fb9f65eae648a
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-14 11:17:24 +0200

    Add basic multithreading for SteamSpy requests

--- adapters/steamspy_api_adapter.py
+++ adapters/steamspy_api_adapter.py
@@ -5,4 +5,5 @@ import json
 
 def get_game_info(game_id: int) -> dict:
     response = get(f'{config.STEAMSPY_URL}/api.php?request=appdetails&appid={game_id}')
+    response.raise_for_status()
     return json.loads(response.text)
--- logic/player_stats_creator.py
+++ logic/player_stats_creator.py
@@ -1,11 +1,27 @@
 from config import config
 from adapters import steam_api_adapter, steamspy_api_adapter
+from logic.get_game_info_thread import GameInfoFinder
+
+
+def _get_games_info(games: list) -> dict:
+    threads = []
+    for game in games:
+        threads.append(GameInfoFinder(game['appid']))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    games_info = {}
+    for thread in threads:
+        games_info[thread.game_info['appid']] = thread.game_info
+    return games_info
 
 
 def _create_stats_by_common_game_info_attribute(games: list, attribute: str) -> dict:
+    games_info = _get_games_info(games)
     stats_dict = {}
     for game_stats in games:
-        game_info = steamspy_api_adapter.get_game_info(game_stats['appid'])
+        game_info = games_info['appid']
         if game_stats['playtime_forever'] != 0:
             if game_info[attribute] not in stats_dict:
                 stats_dict[game_info[attribute]] = 0
