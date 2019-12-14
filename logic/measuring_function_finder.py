56b629c5f9b08d1c8351c5eb2daebc863d06bddb
Jonathan Rosenboim <masterfqf@gmail.com>
2019-12-14 11:37:27 +0200

    Improve error output.

--- logic/measuring_function_finder.py
+++ logic/measuring_function_finder.py
@@ -13,4 +13,4 @@ def get_stats_measuring_function(measure_by: str) -> Callable:
         return measuring_stats_dict[measure_by]
     except KeyError:
         raise Exception(f'Cannot measure by parameter "{measure_by}", must be one of the '
-                        f'following: {measuring_stats_dict.keys()}')
+                        f'following: {list(measuring_stats_dict.keys())}')
