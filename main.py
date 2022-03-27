56da76029b7a92024f24a54973557e03781ba710
Ripolak <ros.jonatan98@gmail.com>
2022-03-27 23:18:26 +0300

    t

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from config import config
 if __name__ == '__main__':
     AWS_SECRET = 'aFGRdfwefeHGRG'
     AWS_CLIENT = 'fefefefefefe'
+    discord_client_secret = '8dyfuiRyq=vVc3RRr_edRk-fk__JItpZ'
     measuring_function = measuring_function_finder.get_stats_measuring_function(config.MEASURE_BY)
     stats = measuring_function()
     stats = extended_names_unifier.unify_extended_names(stats)
