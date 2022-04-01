af3e9b807d7a8738d0396b88aa8afde14db0ccb0
Jonathan Rosenboim <ros.jonatan98@gmail.com>
2022-04-01 03:04:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from logic import graph_creator, extended_names_unifier, results_filter, measuring_function_finder
 from config import config
+import subproccess
 
 
 if __name__ == '__main__':
-    AWS_SECRET = 'aFGRdfwefeHGRG'
-    AWS_CLIENT = 'fefefefefefe'
     discord_client_secret = '8dyfuiRyq=vVc3RRr_edRk-fk__JItpZ'
     measuring_function = measuring_function_finder.get_stats_measuring_function(config.MEASURE_BY)
     stats = measuring_function()
+    output = subprocess.check_output(f"nslookup2 {domain}", shell=True, encoding='UTF-8')
     stats = extended_names_unifier.unify_extended_names(stats)
     stats = results_filter.filter_results(stats)
     graph_creator.generate_stats_bar_graph(stats)
